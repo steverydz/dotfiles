0b5d055f79f7783105bb783689e3d638fa14e855
Steve Rydz <steverydz@gmail.com>
2019-03-06 10:02:46 +0000

    Update display configuration

--- .screenlayout/dual.sh
+++ .screenlayout/dual.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP-1 --off --output eDP-1 --mode 1280x720 --pos 1920x0 --rotate normal --output DP-2 --off
+xrandr --output DP-1 --off --output eDP-1 --mode 1600x900 --pos 1920x0 --rotate normal --output DP-2 --off
